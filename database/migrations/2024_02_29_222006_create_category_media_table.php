44d263278ebd57d3cbab2c171f978a483a365e1f
layton <layton.berth@student.howest.be>
2024-03-01 00:30:37 +0100

    Add category_id and image_url columns to category_media table

--- database/migrations/2024_02_29_222006_create_category_media_table.php
+++ database/migrations/2024_02_29_222006_create_category_media_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('category_media', function (Blueprint $table) {
             $table->id();
+            $table->integer('category_id');
+            $table->string('image_url');
             $table->timestamps();
         });
     }
