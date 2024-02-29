b6e3f1bda2e337615fe0de38941c8172e251a8e4
layton <layton.berth@student.howest.be>
2024-03-01 00:51:08 +0100

    Add customer_id and product_id columns to wishlists table

--- database/migrations/2024_02_29_222314_create_wishlists_table.php
+++ database/migrations/2024_02_29_222314_create_wishlists_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('wishlists', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('customer_id');
+            $table->unsignedBigInteger('product_id');
             $table->timestamps();
         });
     }
