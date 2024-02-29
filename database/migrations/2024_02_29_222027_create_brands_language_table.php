0abea6a1ba32f66087102cc867dc369df39f8e32
layton <layton.berth@student.howest.be>
2024-03-01 00:32:06 +0100

    Add columns to brands_language table

--- database/migrations/2024_02_29_222027_create_brands_language_table.php
+++ database/migrations/2024_02_29_222027_create_brands_language_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('brands_language', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('brand_id');
+            $table->unsignedBigInteger('language_id');
+            $table->string('name');
+            $table->string('description');
             $table->timestamps();
         });
     }
