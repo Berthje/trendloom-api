349e14ba99c191981768584d0cfd0ea4d64ac9f8
layton <layton.berth@student.howest.be>
2024-03-01 00:07:23 +0100

    Add name, description, and parent_category_id columns to categories table

--- database/migrations/2024_02_29_221947_create_categories_table.php
+++ database/migrations/2024_02_29_221947_create_categories_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string("name");
+            $table->string("description");
+            $table->string("parent_category_id")->nullable();
             $table->timestamps();
         });
     }
