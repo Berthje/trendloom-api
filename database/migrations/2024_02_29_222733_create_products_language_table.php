678b00733fb67f81be32267a044273d034e31f4d
layton <layton.berth@student.howest.be>
2024-03-01 00:38:56 +0100

    Add columns to products_language table

--- database/migrations/2024_02_29_222733_create_products_language_table.php
+++ database/migrations/2024_02_29_222733_create_products_language_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('products_language', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->unsignedBigInteger('product_id');
+            $table->unsignedBigInteger('language_id');
+            $table->decimal('price', 8, 2);
+            $table->json('tags')->nullable();
             $table->timestamps();
         });
     }
