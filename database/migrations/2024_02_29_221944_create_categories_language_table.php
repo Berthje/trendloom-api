5800c12a38c1e14e6aae354ad59d5988380f8537
layton <layton.berth@student.howest.be>
2024-03-01 00:30:17 +0100

    Add columns to categories_language table

--- database/migrations/2024_02_29_221944_create_categories_language_table.php
+++ database/migrations/2024_02_29_221944_create_categories_language_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('categories_language', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->integer('category_id');
+            $table->string('language_id');
             $table->timestamps();
         });
     }
