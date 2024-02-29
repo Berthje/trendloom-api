499db5bc231d3a757f429d86045824a974d4b8a0
layton <layton.berth@student.howest.be>
2024-03-01 00:50:46 +0100

    Add columns to customers table

--- database/migrations/2024_02_29_222335_create_customers_table.php
+++ database/migrations/2024_02_29_222335_create_customers_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email')->unique();
+            $table->string('phone_number')->nullable();
+            $table->string('password');
+            $table->unsignedBigInteger('address_id');
             $table->timestamps();
         });
     }
