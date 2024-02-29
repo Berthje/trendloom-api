7ebf9b4ea69822e079ffcc1bca5a385e0c2c7130
layton <layton.berth@student.howest.be>
2024-03-01 00:05:19 +0100

    Add name, description, and logo_url columns to brands table

--- database/migrations/2024_02_29_221935_create_brands_table.php
+++ database/migrations/2024_02_29_221935_create_brands_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('brands', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->string('logo_url');
             $table->timestamps();
         });
     }
