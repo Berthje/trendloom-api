37637e563ae5bcf5f97320d59cd4841e388f2fd3
layton <layton.berth@student.howest.be>
2024-03-01 00:49:13 +0100

    Add fields to the orders table

--- database/migrations/2024_02_29_222422_create_orders_table.php
+++ database/migrations/2024_02_29_222422_create_orders_table.php
@@ -13,6 +13,16 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('customer_id');
+            $table->unsignedBigInteger('address_id');
+            $table->unsignedBigInteger('coupon_id')->nullable();
+            $table->date('order_date');
+            $table->enum('status', ['shipping', 'paid', 'cancelled', 'delivered', 'returned', 'refunded']);
+            $table->integer('amount_products');
+            $table->decimal('total_price', 8, 2);
+            $table->string('payment_method');
+            $table->string('shipping_method');
+            $table->string('tracking_number')->nullable();
             $table->timestamps();
         });
     }
