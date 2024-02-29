a738d3a1306bd6ef4eae3513575548f104138365
layton <layton.berth@student.howest.be>
2024-03-01 00:37:39 +0100

    Add address fields to addresses table

--- database/migrations/2024_02_29_222309_create_addresses_table.php
+++ database/migrations/2024_02_29_222309_create_addresses_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('addresses', function (Blueprint $table) {
             $table->id();
+            $table->string('address');
+            $table->string('city');
+            $table->string('state');
+            $table->string('zip');
+            $table->string('country');
             $table->timestamps();
         });
     }
