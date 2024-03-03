ddb8e4e8f099d6d6ecb328b4394b84febd987bf7
layton <layton.berth@student.howest.be>
2024-03-03 18:20:40 +0100

    Fix bug in login functionality

--- database/migrations/2024_03_03_171546_create_user_roles_table.php
+++ database/migrations/2024_03_03_171546_create_user_roles_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('user_roles', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('customer_id');
             $table->unsignedBigInteger('role_id');
             $table->timestamps();
         });
