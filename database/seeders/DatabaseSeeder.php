963de851f8c4a5464c911026268d26e27bac4447
Layton Berth <laytonbrth@gmail.com>
2024-03-06 19:09:50 +0100

    Add product size and stock seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\ProductStock;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,12 +14,14 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             RoleSeeder::class,
+            ProductSizeSeeder::class,
             LanguageSeeder::class,
             CategorySeeder::class,
             BrandSeeder::class,
             AddressSeeder::class,
             ProductSeeder::class,
             CustomerSeeder::class,
+            ProductStockSeeder::class,
             BrandLanguageSeeder::class,
             ProductLanguageSeeder::class,
             CategoryLanguageSeeder::class,
