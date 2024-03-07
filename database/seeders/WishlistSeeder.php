1bd330555e83be9b7afa66c17748d9270081fc0e
Layton Berth <laytonbrth@gmail.com>
2024-03-07 15:29:04 +0100

    Add CSV data to WishlistSeeder

--- database/seeders/WishlistSeeder.php
+++ database/seeders/WishlistSeeder.php
@@ -4,14 +4,24 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Wishlist;
+use App\Helpers\CsvHandler;
 
 class WishlistSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(CsvHandler $csvHandler): void
     {
-        //
+        $data = $csvHandler->getDataFromCsv('data\csv\wishlists.csv');
+        $model = new Wishlist();
+
+        foreach ($data as $row) {
+            $model->create([
+                'customer_id' => $row['customer_id'],
+                'product_id' => $row['product_id'],
+            ]);
+        }
     }
 }
