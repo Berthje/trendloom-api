49d65617e8ff48b8121372af11a4c7e91ae37c7c
Layton Berth <laytonbrth@gmail.com>
2024-03-07 15:24:33 +0100

    Add CSV data import functionality to ProductMediaSeeder

--- database/seeders/ProductMediaSeeder.php
+++ database/seeders/ProductMediaSeeder.php
@@ -4,14 +4,26 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\ProductMedia;
+use App\Helpers\CsvHandler;
+
 
 class ProductMediaSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(CsvHandler $csvHandler): void
     {
-        //
+        $data = $csvHandler->getDataFromCsv('data\csv\category_media.csv');
+        $model = new ProductMedia();
+
+        foreach ($data as $row) {
+            $model->create([
+                'product_id' => $row['product_id'],
+                'image_url' => $row['image_url'],
+                'is_primary' => $row['is_primary'],
+            ]);
+        }
     }
 }
