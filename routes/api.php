212f87afd3e58e73347a48f1f4fdf9f3eaecf54e
layton <layton.berth@student.howest.be>
2024-03-08 15:58:10 +0100

    Add customer routes

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+/*
+|--------------------------------------------------------------------------
+| Customer Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/customers', [CustomerController::class, 'getAllCustomers']);
+Route::get('/customers/{id}', [CustomerController::class, 'getCustomerById']);
