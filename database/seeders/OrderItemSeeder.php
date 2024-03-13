3f6c1b93af488b352f9a4b0bc0b609977f974f8d
layton <layton.berth@student.howest.be>
2024-03-13 10:52:36 +0100

    Add product details to OrderItemSeeder

--- database/seeders/OrderItemSeeder.php
+++ database/seeders/OrderItemSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\OrderItem;
 use App\Helpers\CsvHandler;
+use App\Models\Product;
 
 class OrderItemSeeder extends Seeder
 {
@@ -16,11 +17,15 @@ class OrderItemSeeder extends Seeder
     {
         $data = $csvHandler->getDataFromCsv('data\csv\order_items.csv');
         $model = new OrderItem();
+        $products = Product::all()->keyBy('id');
 
         foreach ($data as $row) {
+            $product = $products[$row['product_id']];
+
             $model->create([
                 'order_id' => $row['order_id'],
                 'product_id' => $row['product_id'],
+                'product_details' => json_encode($product),
                 'quantity' => $row['quantity'],
                 'size' => $row['size'],
             ]);
