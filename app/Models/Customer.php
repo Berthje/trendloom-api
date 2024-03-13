1b15553dde61d8769abb79320153fabafee22d3d
layton <layton.berth@student.howest.be>
2024-03-13 11:18:58 +0100

    Add fillable, hidden, and casts properties to Customer model

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -9,6 +9,14 @@ class Customer extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['first_name', 'last_name', 'email', 'phone_number', 'password', 'address_id'];
+
+    protected $hidden = ['password'];
+
+    protected $casts = [
+        'password' => 'hashed',
+    ];
+
     public function roles()
     {
         return $this->belongsToMany(Role::class, 'user_roles');
