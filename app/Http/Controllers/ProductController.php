0cc6241323a0976f56d0a6df644a21c9ceb74dc7
layton <layton.berth@student.howest.be>
2024-03-15 00:18:15 +0100

    add right import and add folder services

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Modules\Product\Services\ProductService;
+use App\Modules\Products\Services\ProductService;
 
 class ProductController extends ApiServiceController
 {
