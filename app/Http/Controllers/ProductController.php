617451d63d8868b6ee8e7b49deb7b06a66e467c6
layton <layton.berth@student.howest.be>
2024-03-15 00:15:45 +0100

    add all routing functions in the controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,31 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Modules\Product\Services\ProductService;
 
-class ProductController extends Controller
+class ProductController extends ApiServiceController
 {
-    //
+    public function __construct(ProductService $service) {
+        $this->service = $service;
+    }
+
+    public function getAllProducts() {
+        //TODO: implement this method
+    }
+
+    public function createProduct() {
+        //TODO: implement this method
+    }
+
+    public function getProductById($productId) {
+        //TODO: implement this method
+    }
+
+    public function updateProduct($productId) {
+        //TODO: implement this method
+    }
+
+    public function deleteProduct($productId) {
+        //TODO: implement this method
+    }
 }
