a92ec350cda08ae1c6b448054d8dc671cf41754f
layton <layton.berth@student.howest.be>
2024-03-15 19:59:24 +0100

    implement brancontroller

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Modules\Brands\Services\BrandService;
 use Illuminate\Http\Request;
 
-class BrandController extends Controller
+class BrandController extends ApiServiceController
 {
-    //
+    public function __construct(BrandService $service) {
+        $this->service = $service;
+    }
+
+    public function getAllBrands() {
+        return $this->getAll();
+    }
+
+    public function createBrand(Request $request) {
+        return $this->create($request);
+    }
+
+    public function getBrandById($brandId) {
+        return $this->get($brandId);
+    }
+
+    public function updateBrand(Request $request, $brandId) {
+        return $this->update($request, $brandId);
+    }
+
+    public function deleteBrand($brandId) {
+        return $this->delete($brandId);
+    }
 }
