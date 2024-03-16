1c977232f979f45370ffe538d163f994174aca53
Layton Berth <laytonbrth@gmail.com>
2024-03-16 14:21:44 +0100

    Refactor BrandController methods

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -7,27 +7,33 @@ use Illuminate\Http\Request;
 
 class BrandController extends ApiServiceController
 {
-    public function __construct(BrandService $service) {
+    public function __construct(BrandService $service)
+    {
         $this->service = $service;
     }
 
-    public function getAllBrands() {
+    public function getAllBrands()
+    {
         return $this->getAll();
     }
 
-    public function createBrand(Request $request) {
+    public function createBrand(Request $request)
+    {
         return $this->create($request);
     }
 
-    public function getBrandById($brandId) {
+    public function getBrandById($brandId)
+    {
         return $this->get($brandId);
     }
 
-    public function updateBrand(Request $request, $brandId) {
+    public function updateBrand(Request $request, $brandId)
+    {
         return $this->update($request, $brandId);
     }
 
-    public function deleteBrand($brandId) {
+    public function deleteBrand($brandId)
+    {
         return $this->delete($brandId);
     }
 }
