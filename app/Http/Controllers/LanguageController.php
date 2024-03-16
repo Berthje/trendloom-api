f5bc5584b3832c086b34e97a973942600d2c148d
Layton Berth <laytonbrth@gmail.com>
2024-03-16 15:17:21 +0100

    Refactor LanguageController to use LanguageService***

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -2,9 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Modules\Languages\Services\LanguageService;
 use Illuminate\Http\Request;
 
-class LanguageController extends Controller
+class LanguageController extends ApiServiceController
 {
-    //
+    public function __construct(LanguageService $service)
+    {
+        $this->service = $service;
+    }
+
+    public function getAllLanguages()
+    {
+        return $this->getAll();
+    }
+
+    public function createLanguage(Request $request)
+    {
+        return $this->create($request);
+    }
+
+    public function getLanguageById($languageId)
+    {
+        return $this->get($languageId);
+    }
+
+    public function updateLanguage(Request $request, $languageId)
+    {
+        return $this->update($request, $languageId);
+    }
+
+    public function deleteLanguage($languageId)
+    {
+        return $this->delete($languageId);
+    }
 }
