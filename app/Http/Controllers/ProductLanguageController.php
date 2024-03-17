515fae403861435fa695f5a92c5bca11cfb96b9d
Layton Berth <laytonbrth@gmail.com>
2024-03-17 22:54:13 +0100

    Refactor ProductLanguageController to use ProductLanguageService

--- app/Http/Controllers/ProductLanguageController.php
+++ app/Http/Controllers/ProductLanguageController.php
@@ -2,9 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Modules\ProductLanguages\Services\ProductLanguageService;
 use Illuminate\Http\Request;
 
-class ProductLanguageController extends Controller
+class ProductLanguageController extends ApiServiceController
 {
-    //
+    public function __construct(ProductLanguageService $service)
+    {
+        $this->service = $service;
+    }
+
+    public function getAllProductLanguages()
+    {
+        return $this->getAll();
+    }
+
+    public function createProductLanguage(Request $request)
+    {
+        return $this->create($request);
+    }
+
+    public function getProductLanguageById($productLanguageId)
+    {
+        return $this->get($productLanguageId);
+    }
+
+    public function updateProductLanguage(Request $request, $productLanguageId)
+    {
+        return $this->update($request, $productLanguageId);
+    }
+
+    public function deleteBrand($productLanguageId)
+    {
+        return $this->delete($productLanguageId);
+    }
 }
