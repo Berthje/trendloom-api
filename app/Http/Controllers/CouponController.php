356b2288bf01ddf137b64a7c752db7b9a0ac00ec
Layton Berth <laytonbrth@gmail.com>
2024-03-17 22:33:08 +0100

    Add CouponService dependency and update CouponController methods

--- app/Http/Controllers/CouponController.php
+++ app/Http/Controllers/CouponController.php
@@ -3,8 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Modules\Coupons\Services\CouponService;
 
 class CouponController extends Controller
 {
-    //
+    public function __construct(CouponService $service)
+    {
+        $this->service = $service;
+    }
+
+    public function getAllCoupons()
+    {
+        return $this->getAll();
+    }
+
+    public function createCoupon(Request $request)
+    {
+        return $this->create($request);
+    }
+
+    public function getCouponById($couponId)
+    {
+        return $this->get($couponId);
+    }
+
+    public function updateCoupon(Request $request, $couponId)
+    {
+        return $this->update($request, $couponId);
+    }
+
+    public function deleteCoupon($couponId)
+    {
+        return $this->delete($couponId);
+    }
 }
