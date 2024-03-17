aff827ef1a8f488b38b5f74374271130ddcd3a0b
Layton Berth <laytonbrth@gmail.com>
2024-03-17 22:31:53 +0100

    Refactor RoleController to use RoleService

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -2,9 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Modules\Roles\Services\RoleService;
 use Illuminate\Http\Request;
 
-class RoleController extends Controller
+class RoleController extends ApiServiceController
 {
-    //
+    public function __construct(RoleService $service)
+    {
+        $this->service = $service;
+    }
+
+    public function getAllRoles()
+    {
+        return $this->getAll();
+    }
+
+    public function createRole(Request $request)
+    {
+        return $this->create($request);
+    }
+
+    public function getRoleById($roleId)
+    {
+        return $this->get($roleId);
+    }
+
+    public function updateRole(Request $request, $roleId)
+    {
+        return $this->update($request, $roleId);
+    }
+
+    public function deleteRole($roleId)
+    {
+        return $this->delete($roleId);
+    }
 }
