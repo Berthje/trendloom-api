2bf159c673187f33a2d0146f840964f87d6cfde2
Layton Berth <laytonbrth@gmail.com>
2024-03-18 20:54:14 +0100

    Refactor BrandLanguageController to use BrandLanguageService

--- app/Http/Controllers/BrandLanguageController.php
+++ app/Http/Controllers/BrandLanguageController.php
@@ -2,9 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Modules\BrandLanguages\Services\BrandLanguageService;
 use Illuminate\Http\Request;
 
-class BrandLanguageController extends Controller
+class BrandLanguageController extends ApiServiceController
 {
-    //
+    public function __construct(BrandLanguageService $service)
+    {
+        $this->service = $service;
+    }
+
+    public function getAllBrandLanguages()
+    {
+        return $this->getAll();
+    }
+
+    public function createBrandLanguage(Request $request)
+    {
+        return $this->create($request);
+    }
+
+    public function getBrandLanguageById($brandLanguageId)
+    {
+        return $this->get($brandLanguageId);
+    }
+
+    public function updateBrandLanguage(Request $request, $brandLanguageId)
+    {
+        return $this->update($request, $brandLanguageId);
+    }
+
+    public function deleteBrandLanguage($brandLanguageId)
+    {
+        return $this->delete($brandLanguageId);
+    }
 }
