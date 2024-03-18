86b874fcc1904e6e9a2bbb8e07c1b5efee5c65c3
Layton Berth <laytonbrth@gmail.com>
2024-03-18 19:24:35 +0100

    Fix bug in login functionality

---
+++ app/Modules/CategoryLanguages/Services/CategoryLanguageService.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Modules\CategoryLanguages\Services;
+
+use App\Models\CategoryLanguage;
+use App\Modules\Core\Services\Service;
+
+class CategoryLanguageService extends Service {
+    protected $fields= ['name', 'description', 'category_id', 'language_id'];
+    protected $searchField = 'categoryLanguage';
+
+    public function __construct(CategoryLanguage $model) {
+        parent::__construct($model);
+    }
+
+    protected $rules = [
+        "add" => [
+            'name' => 'required|string',
+            'description' => 'required|string',
+            'category_id' => 'required|exists:categories,id',
+            'language_id' => 'required|exists:languages,id'
+        ],
+        "update" => [
+            'name' => 'sometimes|string',
+            'description' => 'sometimes|string',
+            'category_id' => 'sometimes|exists:categories,id',
+            'language_id' => 'sometimes|exists:languages,id'
+        ],
+        "delete" => [
+            'id' => 'required|exists:category_languages,id',
+        ],
+        "get" => [
+            'id' => 'required|exists:category_languages,id',
+        ]
+    ];
+}
