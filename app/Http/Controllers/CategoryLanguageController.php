64835e19bbcaeee9bf139a24e9d4290d3ac1fe3d
Layton Berth <laytonbrth@gmail.com>
2024-03-18 19:22:58 +0100

    Refactor CategoryLanguageController to use CategoryLanguageService

--- app/Http/Controllers/CategoryLanguageController.php
+++ app/Http/Controllers/CategoryLanguageController.php
@@ -2,9 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Modules\CategoryLanguages\Services\CategoryLanguageService;
 use Illuminate\Http\Request;
 
-class CategoryLanguageController extends Controller
+class CategoryLanguageController extends ApiServiceController
 {
-    //
+    public function __construct(CategoryLanguageService $service)
+    {
+        $this->service = $service;
+    }
+
+    public function getAllCategoryLanguages()
+    {
+        return $this->getAll();
+    }
+
+    public function createCategoryLanguage(Request $request)
+    {
+        return $this->create($request);
+    }
+
+    public function getCategoryLanguageById($categoryLanguageId)
+    {
+        return $this->get($categoryLanguageId);
+    }
+
+    public function updateCategoryLanguage(Request $request, $categoryLanguageId)
+    {
+        return $this->update($request, $categoryLanguageId);
+    }
+
+    public function deleteCategoryLanguage($categoryLanguageId)
+    {
+        return $this->delete($categoryLanguageId);
+    }
 }
