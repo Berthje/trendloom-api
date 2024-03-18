328eacb0f0f686979bb324b146244578b0f9483a
layton <layton.berth@student.howest.be>
2024-03-18 10:23:59 +0100

    Refactor ProductSizeController to use ProductSizeService

--- app/Http/Controllers/ProductSizeController.php
+++ app/Http/Controllers/ProductSizeController.php
@@ -2,9 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Modules\ProductSizes\Services\ProductSizeService;
 use Illuminate\Http\Request;
 
-class ProductSizeController extends Controller
+class ProductSizeController extends ApiServiceController
 {
-    //
+    public function __construct(ProductSizeService $service)
+    {
+        $this->service = $service;
+    }
+
+    public function getAllProductSizes()
+    {
+        return $this->getAll();
+    }
+
+    public function createProductSiz(Request $request)
+    {
+        return $this->create($request);
+    }
+
+    public function getProductSizById($productSize)
+    {
+        return $this->get($productSize);
+    }
+
+    public function updateProductSiz(Request $request, $productSize)
+    {
+        return $this->update($request, $productSize);
+    }
+
+    public function deleteProductSiz($productSize)
+    {
+        return $this->delete($productSize);
+    }
 }
