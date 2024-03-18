50131630306f887d28b09d9b6bb4a68b5f235eb6
Layton Berth <laytonbrth@gmail.com>
2024-03-18 22:50:07 +0100

    Refactor CategoryCouponController to use CategoryCouponService

--- app/Http/Controllers/CategoryCouponController.php
+++ app/Http/Controllers/CategoryCouponController.php
@@ -2,9 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Modules\CategoryCoupons\Services\CategoryCouponService;
 use Illuminate\Http\Request;
 
-class CategoryCouponController extends Controller
+class CategoryCouponController extends ApiServiceController
 {
-    //
+    public function __construct(CategoryCouponService $service)
+    {
+        $this->service = $service;
+    }
+
+    public function getAllCategoryCoupons()
+    {
+        return $this->getAll();
+    }
+
+    public function createCategoryCoupon(Request $request)
+    {
+        return $this->create($request);
+    }
+
+    public function getCategoryCouponById($categoryCouponId)
+    {
+        return $this->get($categoryCouponId);
+    }
+
+    public function updateCategoryCoupon(Request $request, $categoryCouponId)
+    {
+        return $this->update($request, $categoryCouponId);
+    }
+
+    public function deleteCategoryCoupon($categoryCouponId)
+    {
+        return $this->delete($categoryCouponId);
+    }
 }
