2276c5ca855fec4ed6dd549ae63a76d5f4e3b8b2
layton <layton.berth@student.howest.be>
2024-03-18 11:37:53 +0100

    Refactor OrderItemController to extend ApiServiceController

--- app/Http/Controllers/OrderItemController.php
+++ app/Http/Controllers/OrderItemController.php
@@ -4,7 +4,39 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class OrderItemController extends Controller
+class OrderItemController extends ApiServiceController
 {
-    //
+    public function __construct(OrderItemService $service)
+    {
+        $this->service = $service;
+    }
+
+    public function getAllOrderItems()
+    {
+        return $this->getAll();
+    }
+
+    public function createOrderItem(Request $request)
+    {
+        return $this->create($request);
+    }
+
+    public function getOrderItemById($orderItemId)
+    {
+        return $this->get($orderItemId);
+    }
+
+    public function updateOrderItem(Request $request, $orderItemId)
+    {
+        return $this->update($request, $orderItemId);
+    }
+
+    public function deleteOrderItem($orderItemId)
+    {
+        return $this->delete($orderItemId);
+    }
+
+    public function getOrderByOrderItemId($orderItemId) {
+        return $this->service->getOrderByOrderItemId($orderItemId);
+    }
 }
