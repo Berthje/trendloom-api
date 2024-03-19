26182c19932b4bea353df9979f7035ab18e9645b
Layton Berth <laytonbrth@gmail.com>
2024-03-19 01:05:13 +0100

    Add BrandCouponService dependency and update BrandCouponController methods

--- app/Http/Controllers/BrandCouponController.php
+++ app/Http/Controllers/BrandCouponController.php
@@ -2,9 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Modules\BrandCoupons\Services\BrandCouponService;
 use Illuminate\Http\Request;
 
 class BrandCouponController extends Controller
 {
-    //
+    public function __construct(BrandCouponService $service)
+    {
+        $this->service = $service;
+    }
+
+    public function getAllBrandCoupons()
+    {
+        return $this->getAll();
+    }
+
+    public function createBrandCoupon(Request $request)
+    {
+        return $this->create($request);
+    }
+
+    public function getBrandCouponById($brandCouponId)
+    {
+        return $this->get($brandCouponId);
+    }
+
+    public function updateBrandCoupon(Request $request, $brandCouponId)
+    {
+        return $this->update($request, $brandCouponId);
+    }
+
+    public function deleteBrandCoupon($brandCouponId)
+    {
+        return $this->delete($brandCouponId);
+    }
 }
