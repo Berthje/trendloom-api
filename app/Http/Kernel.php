45bedb81c6957822a738b62d3ce9466c6a07eac7
Layton Berth <laytonbrth@gmail.com>
2024-03-22 00:24:29 +0100

    Update auth.csrf.jwt middleware in Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,6 +64,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'auth.csrf.jwt' => \App\Http\Middleware\VerifyJwtCsrfToken::class, 
+        'auth.csrf.jwt' => \App\Http\Middleware\VerifyJwtCsrfToken::class,
     ];
 }
