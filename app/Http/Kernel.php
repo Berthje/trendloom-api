2e640c53f08f7c74127a13450843cb0bfe25cc89
Layton Berth <laytonbrth@gmail.com>
2024-03-21 23:15:23 +0100

    Add JWT authentication support***
    ***Add JWT authentication support to the auth configuration and composer.json file. Also, register JWT middleware in the Http/Kernel.php file.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,8 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'Jwt' => Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
+        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
+        'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
     ];
 }
