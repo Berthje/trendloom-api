e5c0be04e12a67868f7d74bb5634b96f30908085
Layton Berth <laytonbrth@gmail.com>
2024-03-28 00:47:54 +0100

    Remove timestamps from migration files

--- database/migrations/2024_03_03_171546_create_customer_roles_table.php
+++ database/migrations/2024_03_03_171546_create_customer_roles_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('customer_id');
             $table->unsignedBigInteger('role_id');
-            $table->timestamps();
         });
     }
 
