e08056cdd854a7d93ace1b0212cd4de112b4a2aa
Layton Berth <laytonbrth@gmail.com>
2024-04-18 00:55:55 +0200

    Refactor CategoryFrontService to add methods for retrieving category and product data

--- app/Modules/Categories/Services/CategoryFrontService.php
+++ app/Modules/Categories/Services/CategoryFrontService.php
@@ -20,4 +20,47 @@ class CategoryFrontService extends FrontService
             ->join('languages', 'languages.id', '=', 'category_languages.language_id')
             ->select('categories.*', 'languages.*', 'category_languages.*');
     }
+
+    public function getCategoryById($request, $categoryId)
+    {
+        $query = $this->getTranslationQuery()
+            ->where('categories.id', $categoryId);
+
+        if ($request->has('lang')) {
+            $query->where('languages.code', $request->input('lang'));
+        }
+
+        $category = $query->first();
+
+        return $category;
+    }
+
+    public function getProductsByCategoryId($request, $categoryId)
+    {
+        $category = $this->model
+            ->with(['products', 'children.products'])
+            ->where('id', $categoryId)
+            ->first();
+
+        $products = $category->products;
+
+        $products = $products->concat($this->getProductsFromChildren($category->children));
+
+        return $products;
+    }
+
+    private function getProductsFromChildren($children)
+    {
+        $products = collect();
+
+        foreach ($children as $child) {
+            $products = $products->concat($child->products);
+
+            if ($child->children->isNotEmpty()) {
+                $products = $products->concat($this->getProductsFromChildren($child->children));
+            }
+        }
+
+        return $products;
+    }
 }
