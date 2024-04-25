e192053d0d65dc587c10ed6598c13ea63a996303
Layton Berth <laytonbrth@gmail.com>
2024-04-25 14:22:01 +0200

    Add create method to ProductService for creating products with translations

--- app/Modules/Products/Services/ProductService.php
+++ app/Modules/Products/Services/ProductService.php
@@ -3,6 +3,7 @@ namespace App\Modules\Products\Services;
 
 use App\Models\Product;
 use App\Modules\Core\Services\Service;
+use App\Models\Language;
 
 class ProductService extends Service {
     protected $fields= ['name', 'description', 'price', 'sku', 'status', 'ean_barcode', 'brand_id', 'category_id'];
@@ -45,5 +46,37 @@ class ProductService extends Service {
             'brand:id,name,description,logo_url',
             'category:id,name,description,parent_category_id'
         ];
+    }
+
+    public function create($data, $ruleKey = "add") {
+        $this->validate($data, $ruleKey);
+
+        if ($this->HasErrors()) {
+            return;
+        }
+
+        $availableLanguages = Language::all()->pluck('code')->toArray();
+
+        if (!isset($data['languages']) || count(array_intersect($availableLanguages, array_keys($data['languages']))) !== count($availableLanguages)) {
+            return response()->json(['error' => 'All available languages must be provided.'], 400);
+        }
+
+        $product = $this->model->create($data);
+
+        foreach ($data['languages'] as $languageCode => $translationData) {
+            $language = Language::where('code', $languageCode)->first();
+
+            if ($language) {
+                $product->translations()->create([
+                    'language_id' => $language->id,
+                    'name' => $translationData['name'],
+                    'description' => $translationData['description'],
+                    'price' => $translationData['price'],
+                    'tags' => $translationData['tags'],
+                ]);
+            }
+        }
+
+        return $product;
     }
 }
