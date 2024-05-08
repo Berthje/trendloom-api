5c9f36529e80112479ef280f3c2e646f90011288
Layton Berth <laytonbrth@gmail.com>
2024-05-08 02:40:43 +0200

    chore: Update TranslatableService to include updateTranslations method

--- app/Modules/Core/Services/TranslatableService.php
+++ app/Modules/Core/Services/TranslatableService.php
@@ -32,4 +32,39 @@ class TranslatableService extends Service {
 
         return $model;
     }
+
+    public function update($id, $data, $ruleKey = "update") {
+        $this->validate($data, $ruleKey);
+
+        if ($this->HasErrors()) {
+            return;
+        }
+
+        $languageService = new LanguageService(new Language());
+
+        if (!$languageService->areLanguagesValid($data)) {
+            return response()->json(['error' => 'All available languages must be provided.'], 400);
+        }
+
+        $modelData = $data;
+        unset($modelData['languages']);
+        $model = $this->model->find($id);
+        $model->update($modelData);
+
+        $this->updateTranslations($model, $data['languages']);
+
+        return $model;
+    }
+
+    public function updateTranslations($model, $translations) {
+        foreach ($translations as $language => $translationData) {
+            $languageId = Language::where('code', $language)->first()->id;
+
+            $translation = $model->translations()->where('language_id', $languageId)->first();
+
+            if ($translation) {
+                $translation->update($translationData);
+            }
+        }
+    }
 }
